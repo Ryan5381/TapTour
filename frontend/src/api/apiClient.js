3e7728018d940ea9f8cd282ab8f48be08c619f98
Ryan <youcen666@gamil.com>
2025-02-17 04:18:40 +0800

    Fix image path for background in Netlify deployment

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://taptour-production.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
-    withCredentials: true
-
+  withCredentials: true,
 })
 
 export default apiClient
